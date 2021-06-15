5c94366942351d7e113dfe540afcedc75926cd5a
Sarpong Abasimi <abasimisarpong@rocketmail.com>
2021-06-15 18:37:27 +0100

    [SA] Un po di me

--- my-app/src/components/Name/Name.js
+++ my-app/src/components/Name/Name.js
@@ -3,6 +3,15 @@ import React from 'react';
 
 export const Name = ({dataprop})=> {
     return <div>
-        <h1>{dataprop.name}</h1>
+        <h1>La mia storia 🎬</h1>
+        <p><span>Name:</span> {dataprop.name} 👑</p>
+        <p><span>Country:</span> {dataprop.country} 🇬🇭</p>
+        <p><span>Job: </span>{dataprop.occupation} 👨🏿‍💻</p>
+        <a href={dataprop.Youtube}><span>Youtube</span></a>
+        <hr/>
+        <h1>Companies.</h1>
+        {dataprop.previousCompany && dataprop.previousCompany.map((eachCompany,id) => {
+             return(<li key={id}>{eachCompany}</li>)
+         })}
         </div>
 }
